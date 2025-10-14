fc56b4627a8b7a5a1a48b4b58cf26866fa3fb2ff
ohinhyuk <8156217@naver.com>
2025-10-15 00:21:09 +0900

    [chapter1] 4단계 format 변수 제거하기

--- chapter1/index.js
+++ chapter1/index.js
@@ -3,23 +3,17 @@ const statement = (invoice, plays) => {
   let volumneCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
-  const format = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-    minimumFractionDigits: 2,
-  }).format;
-
   for (let perf of invoice.performances) {
     volumneCredits += volumneCreditsFor(perf);
 
     // 청구 내역을 출력한다.
-    result += ` ${playFor(perf).name}: ${format(amountFor(perf) / 100)} (${
+    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
     totalAmount += amountFor(perf);
   }
 
-  result += `총액: ${format(totalAmount / 100)}\n`;
+  result += `총액: ${usd(totalAmount)}\n`;
   result += `적립 포인트: ${volumneCredits}점\n`;
 
   return result;
@@ -58,6 +52,14 @@ const statement = (invoice, plays) => {
       reult += Math.floor(aPerformance.audience / 5);
 
     return reult;
+  }
+
+  function usd(aNumber) {
+    return new Intl.NumberFormat("en-US", {
+      style: "currency",
+      currency: "USD",
+      minimumFractionDigits: 2,
+    }).format(aNumber / 100);
   }
 };
 
