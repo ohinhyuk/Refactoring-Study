6c6d5d3d8d7066b0a53f484324cf8e1bffc4a0fc
ohinhyuk <8156217@naver.com>
2025-10-15 00:46:39 +0900

    [chapter1] 6단계 totalAmount 변수 제거하기 - volumeCredits 변수 제거와 과정 동일

--- chapter1/index.js
+++ chapter1/index.js
@@ -1,5 +1,4 @@
 const statement = (invoice, plays) => {
-  let totalAmount = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
 
   for (let perf of invoice.performances) {
@@ -7,10 +6,9 @@ const statement = (invoice, plays) => {
     result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${
       perf.audience
     }석)\n`;
-    totalAmount += amountFor(perf);
   }
 
-  result += `총액: ${usd(totalAmount)}\n`;
+  result += `총액: ${usd(totalAmount())}\n`;
   result += `적립 포인트: ${totalVolumneCredits()}점\n`;
 
   return result;
@@ -63,6 +61,14 @@ const statement = (invoice, plays) => {
     let result = 0;
     for (let perf of invoice.performances) {
       result += volumneCreditsFor(perf);
+    }
+    return result;
+  }
+
+  function totalAmount() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += amountFor(perf);
     }
     return result;
   }
