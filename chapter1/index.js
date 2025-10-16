a2dd87487acc1a95f47600121f86e29e25644ccb
ohinhyuk <8156217@naver.com>
2025-10-16 23:49:10 +0900

    [chapter1] 7.5단계 html 버전 작성 및 코드 정리

--- chapter1/index.js
+++ chapter1/index.js
@@ -1,102 +1,47 @@
+const createStatementData = require("./createStatementData");
+
 const statement = (invoice, plays) => {
   return renderPlainText(createStatementData(invoice, plays));
-
-  function createStatementData(invoice, plays) {
-    const statementData = {};
-    statementData.customer = invoice.customer;
-    statementData.performances = invoice.performances;
-
-    statementData.performances =
-      statementData.performances.map(enrichPerformance);
-
-    statementData.totalAmount = totalAmount(statementData);
-    statementData.totalVolumne = totalVolumneCredits(statementData);
-
-    return statementData;
-
-    function enrichPerformance(aPerformance) {
-      const result = Object.assign({}, aPerformance);
-
-      result.play = playFor(aPerformance).name;
-      result.amount = amountFor(aPerformance);
-      result.volumeCredits = volumneCreditsFor(aPerformance);
-
-      return result;
-    }
-
-    function playFor(aPerformance) {
-      return plays[aPerformance.playID];
-    }
-
-    function amountFor(aPerformance) {
-      let result = 0;
-      switch (playFor(aPerformance).type) {
-        case "tragedy": // 비극
-          result += 40000;
-          if (aPerformance.audience > 30) {
-            result += 1000 * (aPerformance.audience - 30);
-          }
-          break;
-        case "comedy": // 희극
-          result = 30000;
-          if (aPerformance.audience > 20) {
-            result += 10000 + 500 * (aPerformance.audience - 20);
-          }
-          result += 300 * aPerformance.audience;
-          break;
-        default:
-          throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
-      }
-      return result;
-    }
-
-    function volumneCreditsFor(aPerformance) {
-      let reult = 0;
-
-      reult += Math.max(aPerformance.audience - 30, 0);
-      if ("comedy" === playFor(aPerformance).type)
-        reult += Math.floor(aPerformance.audience / 5);
-
-      return reult;
-    }
-
-    function totalAmount(data) {
-      let result = 0;
-      for (let perf of data.performances) {
-        result += perf.amount;
-      }
-      return result;
-    }
-
-    function totalVolumneCredits(data) {
-      let result = 0;
-      for (let perf of data.performances) {
-        result += perf.volumeCredits;
-      }
-      return result;
-    }
-  }
-
-  function renderPlainText(data) {
-    let result = `청구 내역 (고객명: ${data.customer})\n`;
-
-    for (let perf of data.performances) {
-      result += ` ${perf.play}: ${usd(perf.amount)} (${perf.audience}석)\n`;
-    }
-
-    result += `총액: ${usd(data.totalAmount)}\n`;
-    result += `적립 포인트: ${data.totalVolumne}점\n`;
-
-    return result;
-  }
-
-  function usd(aNumber) {
-    return new Intl.NumberFormat("en-US", {
-      style: "currency",
-      currency: "USD",
-      minimumFractionDigits: 2,
-    }).format(aNumber / 100);
-  }
 };
+
+function renderPlainText(data) {
+  let result = `청구 내역 (고객명: ${data.customer})\n`;
+
+  for (let perf of data.performances) {
+    result += ` ${perf.play}: ${usd(perf.amount)} (${perf.audience}석)\n`;
+  }
+
+  result += `총액: ${usd(data.totalAmount)}\n`;
+  result += `적립 포인트: ${data.totalVolumne}점\n`;
+
+  return result;
+}
+
+const htmlStatement = (invoice, plays) => {
+  return renderHtml(createStatementData(invoice, plays));
+};
+
+function renderHtml(data) {
+  let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+  result += "<table>\n";
+  result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>\n";
+  for (let perf of data.performances) {
+    result += ` <tr><td>${perf.play}</td><td>(${perf.audience}석)</td><td>\n`;
+    result += `${usd(perf.amount)}</td></tr>\n`;
+  }
+  result += "</table>\n";
+  result += `<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+  result += `<p>적립 포인트: <em>${data.totalVolumne}</em>점</p>\n`;
+
+  return result;
+}
+
+function usd(aNumber) {
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2,
+  }).format(aNumber / 100);
+}
 
 module.exports = statement;
